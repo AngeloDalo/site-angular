eea09f0a446feb9715a9c716c48ca0b38b29e55c
AngeloDalo <angelo251158@gmail.com>
2022-05-10 15:25:56 +0200

    get e post

--- src/app/sidebar/sidebar.component.ts
+++ src/app/sidebar/sidebar.component.ts
@@ -1,4 +1,6 @@
+import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-sidebar',
@@ -7,11 +9,51 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SidebarComponent implements OnInit {
 
+  loading: boolean = false;
+
   lista: string[] = ["lezione1", "lezione2", "lezione3", "lezione4"];
 
-  constructor() { }
+  constructor(public http: HttpClient) { }
 
   ngOnInit(): void {
+    this.loadUser();
+    this.loadEpidsodeData("role27", "Nothing00");
   }
+
+  //carica gli utenti
+  //Observable tipologia di oggetto che fa la chiamata e attende una risposta
+  loadUser() : void {
+    this.loading = true;
+    this.http.get('https://test.craftuniversity.it/api.php?request=users').subscribe(res => {
+      console.log(res);
+      this.loading = false;
+    });
+  }
+  loadEpidsodeData(title: string, username: string): void {
+    this.loading = true;
+    this.http.get('https://test.craftuniversity.it/api.php?request=currentEp&title=' + title + '&username=' + username).subscribe(res => {
+      this.loading = false;
+    });
+  }
+  //attivato solo qunado observable riceve la risposte
+  //res contiene la risposta
+  // callback = (res: any) => {
+  //   console.log(res);
+  // }
+  //Dovrebbe ritornare l'id dell'episodio creato
+  createEpisode(num: number, title: string, descr: string, startrec: string) {
+    this.loading = true;
+    this.http.post("https://test.craftuniversity.it/api.php", {
+      request: "createEpisode",
+      num: num,
+      descr: descr,
+      startrec: startrec,
+      title: title
+    }).subscribe(res => {
+      console.log(res);
+      this.loading = false;
+    })
+  }
+
 
 }
